ae18753821d65857c8139f37b8655b1e184dab2e
brunohca <42281349+brunohca@users.noreply.github.com>
2021-03-19 14:21:33 -0300

    Update 7 - Insert configuration table.sql

--- 2 - Setup database scripts/7 - Insert configuration table.sql
+++ 2 - Setup database scripts/7 - Insert configuration table.sql
@@ -1,3 +1,6 @@
+-- Copyright (c) Microsoft Corporation.
+-- Licensed under the MIT License.
+
 TRUNCATE TABLE [operation].[MetadataTableControl]
 
 -------------------------------------------Green CAB
