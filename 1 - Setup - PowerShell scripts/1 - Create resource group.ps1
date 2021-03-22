83f4fcb93a6c3960d1ab179c9866b8abcabe7def
brunohca <42281349+brunohca@users.noreply.github.com>
2021-03-22 17:18:55 -0300

    Update 1 - Create resource group.ps1

--- 1 - Setup - PowerShell scripts/1 - Create resource group.ps1
+++ 1 - Setup - PowerShell scripts/1 - Create resource group.ps1
@@ -5,9 +5,6 @@
 ###########Create Azure components
 ##################################################################################################################
 
-##Connect to your Azure account
-Connect-AzAccount
-
 ##install AZ module
 if ($PSVersionTable.PSEdition -eq 'Desktop' -and (Get-Module -Name AzureRM -ListAvailable)) {
     Write-Warning -Message ('Az module not installed. Having both the AzureRM and ' +
@@ -16,7 +13,10 @@ if ($PSVersionTable.PSEdition -eq 'Desktop' -and (Get-Module -Name AzureRM -List
     Install-Module -Name Az -AllowClobber -Scope CurrentUser
 }
 
+##Connect to your Azure account
+Connect-AzAccount
+
 ##################################################################################################################
 #Create resource group (feel free to change the resource group name and region):
 #New-AzResourceGroup -Name <String> -Location <String>
-New-AzResourceGroup -Name TransportationCompanyAnalytics -Location "eastus"
+New-AzResourceGroup -Name TransportationCompanyAnalytics2 -Location "eastus"
