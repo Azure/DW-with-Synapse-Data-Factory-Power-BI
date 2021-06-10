93382f5f7da801f38dc880db7784fb620a95ccec
brunohca <42281349+brunohca@users.noreply.github.com>
2021-06-10 12:12:36 -0300

    Change histpry
    Adding code to get StorageAccount Key automatically.

--- 1 - Setup - PowerShell scripts/2 - Create Storage Account.ps1
+++ 1 - Setup - PowerShell scripts/2 - Create Storage Account.ps1
@@ -5,6 +5,10 @@
 ###########Create Azure components
 ##################################################################################################################
 
+## YOU NEED TO FILL IN BELOW SCRIPT:
+## 1) SUBSCRIPTION_ID;
+## 2) Storage account name;
+
 ##install AZ module
 if ($PSVersionTable.PSEdition -eq 'Desktop' -and (Get-Module -Name AzureRM -ListAvailable)) {
     Write-Warning -Message ('Az module not installed. Having both the AzureRM and ' +
@@ -21,6 +25,12 @@ Connect-AzAccount
 install-module Az.Storage
 Import-Module Az.Storage
 
+################################
+## PLEASE ADD YOUR SUBSCRIPTION_ID:
+#Select subscription:
+Select-AzSubscription -SubscriptionId xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+## PLEASE ADD YOUR SUBSCRIPTION_ID:
+
 #Create Data Lake Storage Gen2:
 $ResourceGroupName = "TransportationCompanyAnalytics"
 $Name = "[Storage Account Name]"
@@ -30,21 +40,25 @@ $kind = "StorageV2"
 New-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $Name -Location $Location -SkuName $SkuName -Kind $kind -EnableHierarchicalNamespace 1
 ##################################################################################################################
 
+#Get storage account key
+$StorageAccountKey = (Get-AzStorageAccountKey -ResourceGroupName $ResourceGroupName -AccountName $Name)[0].Value
+Write-Host $StorageAccountKey
 ##################################################################################################################
+
 ##create container in storage account 
-Select-AzSubscription -SubscriptionId xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
-$Name = "[Storage Account Name]"
-$key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"        
-$ctx = New-AzStorageContext -StorageAccountName $Name -StorageAccountKey $key
+$ctx = New-AzStorageContext -StorageAccountName $Name -StorageAccountKey $StorageAccountKey
 $filesystemName = "synapsecontainer"
 New-AzStorageContainer -Context $ctx -Name $filesystemName
 
 ##create container in storage account 
-Select-AzSubscription -SubscriptionId xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
-$Name = "[Storage Account Name]"
-$key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"        
-$ctx = New-AzStorageContext -StorageAccountName $Name -StorageAccountKey $key
+$ctx = New-AzStorageContext -StorageAccountName $Name -StorageAccountKey $StorageAccountKey
 $filesystemName = "nyccabdata"
+New-AzStorageContainer -Context $ctx -Name $filesystemName
+##################################################################################################################
+
+##Create container in data lake
+$ctx = New-AzStorageContext -StorageAccountName $Name -StorageAccountKey $StorageAccountKey
+$filesystemName = "stagearea"
 New-AzStorageContainer -Context $ctx -Name $filesystemName
 ##################################################################################################################
 
@@ -52,26 +66,22 @@ New-AzStorageContainer -Context $ctx -Name $filesystemName
 $filesystemName = "nyccabdata"
 $dirname = "ingestion/"
 New-AzDataLakeGen2Item -Context $ctx -FileSystem $filesystemName -Path $dirname -Directory
+##################################################################################################################
 
 ##create folder in data lake
 $filesystemName = "nyccabdata"
 $dirname = "ingestion/green/"
 New-AzDataLakeGen2Item -Context $ctx -FileSystem $filesystemName -Path $dirname -Directory
+##################################################################################################################
 
 ##create folder in data lake
 $filesystemName = "nyccabdata"
 $dirname = "ingestion/yellow/"
 New-AzDataLakeGen2Item -Context $ctx -FileSystem $filesystemName -Path $dirname -Directory
+##################################################################################################################
 
 ##create folder in data lake
 $filesystemName = "nyccabdata"
 $dirname = "ingestion/lookup/"
 New-AzDataLakeGen2Item -Context $ctx -FileSystem $filesystemName -Path $dirname -Directory
-
-##Create container in data lake
-Select-AzSubscription -SubscriptionId xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
-$Name = "[Storage Account Name]"
-$key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-$ctx = New-AzStorageContext -StorageAccountName $Name -StorageAccountKey $key
-$filesystemName = "stagearea"
-New-AzStorageContainer -Context $ctx -Name $filesystemName
+##################################################################################################################
